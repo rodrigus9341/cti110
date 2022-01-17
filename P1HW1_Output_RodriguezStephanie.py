03a1d1d7b9f41b0cad23be527a293f3f43733645
rodrigus9341 <90780654+rodrigus9341@users.noreply.github.com>
2022-01-17 18:44:50 -0500

    Add files via upload

---
+++ P1HW1_Output_RodriguezStephanie.py
@@ -0,0 +1,17 @@
+user_num = int(input('Enter integer:\n'))
+
+userNumSquared = user_num * user_num
+
+userNumCubed = user_num * user_num * user_num
+
+print("You entered:" , user_num)
+
+print(user_num , "squared is" , userNumSquared)
+
+print("And" , user_num , "cubed is" , userNumCubed , "!!")
+
+user_num2 = int(input('Enter another integer:\n'))
+
+print(user_num, '+' , user_num2 , 'is' , int(user_num) + int(user_num2))
+
+print(user_num , '*' , user_num2 , 'is' , int(user_num) * int(user_num2))
